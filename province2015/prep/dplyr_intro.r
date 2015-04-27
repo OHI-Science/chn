1cb938938a333c4f5caaaea49554cbfed01021e3
ningningj <ningningj@gmail.com>
2015-04-27 15:11:33 -0700

    added comments to dplyr_intro

--- province2015/prep/dplyr_intro.r
+++ province2015/prep/dplyr_intro.r
@@ -1,6 +1,7 @@
 # run this code by chunks to get familiar with chaining (%>%) and dplyr
 # iris is a data set available in R; no need to read in anything
 
+
 library(dplyr)
 
 # the beginning: save the head of the iris dataset into a variable called 'data'
@@ -46,7 +47,7 @@ data = head(iris) %>%
          observer     = 'Professor Jones'); data
 
 
-# arrange: order by the largest area (descending)
+# arrange: order by the largest area (descending) #default is ascending
 data = head(iris) %>%
   select(species      = Species,
          sepal_length = Sepal.Length,
