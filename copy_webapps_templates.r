8d1ae4534c852ee5b581d76bbcd7a06c14bb7628
Ning Jiang <mendes@nceas.ucsb.edu>
2015-09-15 17:15:17 -0700

    Ran copy_webapps_templates.r for WebApp updates, which automatically loaded four brew.md. files

--- copy_webapps_templates.r
+++ copy_webapps_templates.r
@@ -5,14 +5,14 @@
 # template files to your local repository. You will then edit the files using
 # RStudio. When editing, save, commit, and push all files normally and they will
 # automatically be updated on the WebApp at 00:00 GMT every day.
-
+#
 # To edit the equations displayed on the WebApp's goals page, you will edit
 # conf/goals.Rmd. The equations are written in a format similar to LaTex; use
 # current equations as a syntax guide. To view the rendered equations, press the
 # 'Knit HTML' button when working with goals.Rmd.
 # Learn more about LaTex syntax at https://en.wikibooks.org/wiki/LaTeX/Mathematics.
 # Learn about .Rmd formatting at http://shiny.rstudio.com/articles/rmarkdown.html
-
+#
 # To edit the text content that is displayed on your WebApp, edit only the text
 # headers and descriptions; all other information is automatically rendered with
 # the most recent content in rgn_labels.csv, layers.csv, goals.Rmd, and
