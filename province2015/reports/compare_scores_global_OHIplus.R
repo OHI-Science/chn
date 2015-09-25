b9f3a42308de6c1a9b3bb33d6d6f44b5835272ad
jules32 <jstewart@nceas.ucsb.edu>
2015-09-25 16:51:49 -0700

    update chn colors reporting

--- province2015/reports/compare_scores_global_OHIplus.R
+++ province2015/reports/compare_scores_global_OHIplus.R
@@ -17,27 +17,27 @@ v2014.3_url = 'https://raw.githubusercontent.com/OHI-Science/ohi-global/4da6b4a1
 scores_v2014.3 = read.csv(v2014.3_url)
 
 # china's region_id in the global assessment is 209
-scores_global_china = scores_v2014.3 %>%            
+scores_global_china = scores_v2014.3 %>%
   filter(region_id == 209) %>%
   mutate(goal = as.character(goal))
 
 
-## read in chn/province2015 scores         
-local_csv = file.path(wd, 'scores.csv')      
+## read in chn/province2015 scores
+local_csv = file.path(wd, 'scores.csv')
 scores_local = read.csv(local_csv) %>%
   mutate(goal = as.character(goal))
 
 
 ## join OHI+ and global scores ----
-scores_compare = scores_global_china %>%   
-  select(goal, 
+scores_compare = scores_global_china %>%
+  select(goal,
          dimension,
-         score_global = score) %>%         
+         score_global = score) %>%
   left_join(scores_local %>%
               filter(region_id == 0) %>%
-              select(goal, 
-                     dimension, 
-                     score_local = score), 
+              select(goal,
+                     dimension,
+                     score_local = score),
             by = c('goal', 'dimension')) %>%
   filter(dimension %in% c('future', 'score', 'status')) %>%
   mutate(score_global = round(score_global, 1),
@@ -51,12 +51,12 @@ pal = pal[1:9]
 
 # cols = data.frame(goal = scores_compare$goal)
 cols = scores_compare[, 'score_diff']
-cols = assign('score_diff', cut(cols, 
-                                 breaks=c(100, 50, 30, 20, 10, 0, -10, -20, -50, -100), 
+cols = assign('score_diff', cut(cols,
+                                 breaks=c(-100, -50, -30, -20, -10, 0, 10, 20, 50, 100),
                                  include.lowest = TRUE, labels=pal))
- 
-hwrite(scores_compare, 
-       file.path(wd, 'reports', 'compare_scores_global_OHIplus.html'), br=TRUE, center=TRUE, border=0, 
-       row.style=list(goal='text-align:center'), table.style='padding: 10px; margin:20px;', 
+
+hwrite(scores_compare,
+       file.path(wd, 'reports', 'compare_scores_global_OHIplus.html'), br=TRUE, center=TRUE, border=0,
+       row.style=list(goal='text-align:center'), table.style='padding: 10px; margin:20px;',
        col.bgcolor=list(scenario='#fff',dimension='#fff',country='#fff', region_id='#fff', score_diff=cols))
-      
+
