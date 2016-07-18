c990c3af95b0ba4b1ae41ba02cb0242eea1beaa3
jules32 <jstewart@nceas.ucsb.edu>
2016-07-18 11:28:45 +0100

    add explicit devtools::

--- install_ohicore.r
+++ install_ohicore.r
@@ -23,8 +23,8 @@ for (p in c('devtools')){
 
 # install packages
 install.packages(c('zoo', 'psych'))
-install_github('ohi-science/rCharts')
-install_github('ohi-science/ohicore@master_a2015')
+devtools::install_github('ohi-science/rCharts')
+devtools::install_github('ohi-science/ohicore@master_a2015')
 
 # Note: you will get warning messages like the following; this is nothing to worry about.
 # Warning: replacing previous import by ‘plyr::mutate’ when loading ‘ohicore’
